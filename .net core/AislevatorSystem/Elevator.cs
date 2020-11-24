f0da851574b5905069fa2ef11941e5f0df5bd9fb
SnowyPainter <SnowyPainter@gmail.com>
2020-11-25 00:15:36 +0900

    Elevator working(moving) visualized

--- .net core/AislevatorSystem/Elevator.cs
+++ .net core/AislevatorSystem/Elevator.cs
@@ -1,8 +1,26 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace AislevatorSystem
 {
     public class Elevator
     {
+        public int CurrentFloor { get; private set; } = 1;
+        
+        public Elevator(int f)
+        {
+            CurrentFloor = f;
+        }
+
+        public IEnumerable<int> GetMoveList(int floor)
+        {
+            int u = CurrentFloor > floor ? -1 : 1;
+            for (int f = 1; f <= Math.Abs(CurrentFloor - floor); f++)
+                yield return CurrentFloor + (f*u);
+
+            CurrentFloor = floor;
+        }
+
     }
 }
--- .net core/ElevatorApp/MainWindow.xaml.cs
+++ .net core/ElevatorApp/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -13,6 +14,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
+using AislevatorSystem;
 
 namespace ElevatorApp
 {
@@ -23,17 +26,56 @@ namespace ElevatorApp
     {
         public readonly int Stories, ElevatorHeight;
         private int currFloor = 1;
+
+        private Elevator elevator;
+
         public MainWindow()
         {
             InitializeComponent();
-
-            Width = 500;
-            
             Stories = 10;
+            currFloor = Stories; //top
+
+            elevator = new Elevator(currFloor);
+            
             ElevatorHeight = (int)(Height / Stories);
 
             SetElevatorHeight(ElevatorHeight);
-            SetElevatorPosition(currFloor);
+
+            for (int i = 1; i <= Stories; i++)
+            {
+                var callBtn = new Button()
+                {
+                    Name = i == 1 ? "ElevatorBottom" : "",
+                    Tag = i,
+                    Content = $"{i}",
+                    Height = ElevatorHeight,
+                    Width = CallButtonMenu.Width,
+                    Margin = new Thickness(0, this.Height - i * ElevatorHeight, 0, 0),
+                    VerticalAlignment = VerticalAlignment.Top,
+                    HorizontalAlignment = HorizontalAlignment.Stretch
+                };
+
+                callBtn.Click += CallBtn_Click;
+
+                CallButtonMenu.Children.Add(callBtn);
+            }
+        }
+
+        private async void CallBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var f = (int)((Button)sender).Tag;
+            Debug.WriteLine($"Elevator Called at {f}");
+            await Task.Run(new Action(() =>
+            {
+                foreach (int f in elevator.GetMoveList(f))
+                {
+                    Thread.Sleep(100);
+                    Dispatcher.Invoke(DispatcherPriority.Normal, new Action(() =>
+                    {
+                        SetElevatorPosition(f);
+                    }));
+                }
+            }));
         }
 
         private void SetElevatorHeight(int h)
@@ -41,28 +83,19 @@ namespace ElevatorApp
             ElevatorObject.Height = h;
             InteractiveMenu.Height = ElevatorObject.Height;
         }
-
-        private void Window_KeyUp(object sender, KeyEventArgs e)
-        {
-            if(e.Key == Key.Up)
-            {
-                SetElevatorPosition(++currFloor);
-            }
-            else if(e.Key == Key.Down)
-            {
-                SetElevatorPosition(--currFloor);
-            }
-
-            if (e.Key == Key.Escape)
-                this.Close();
-
-            Debug.WriteLine($"CurrFloor {currFloor}");
-        }
-
         private void SetElevatorPosition(int floor)
         {
-            ElevatorObject.Margin = new Thickness(0, Height - (floor * ElevatorHeight),0,0);
+            Debug.WriteLine($"{floor} floor ... ");
+            ElevatorObject.Margin = new Thickness(0, Height - (floor * ElevatorHeight) - 14, 0, 0);
             InteractiveMenu.Margin = ElevatorObject.Margin;
         }
+        private void Window_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Escape)
+                this.Close();
+        }
+
+
+
     }
 }
